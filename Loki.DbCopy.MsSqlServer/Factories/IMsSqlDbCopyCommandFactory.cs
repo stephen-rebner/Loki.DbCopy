fc61b035c6adfaca81348f3e311d385bc8f30cd5
stephen-rebner <stephenrebner@gmail.com>
2024-03-26 15:09:51 +0000

    Created IMsSqlDbCopyCommandFactory.cs and MsSqlDbCopyCommandFactory.cs

---
+++ Loki.DbCopy.MsSqlServer/Factories/IMsSqlDbCopyCommandFactory.cs
@@ -0,0 +1,12 @@
+﻿using Loki.DbCopy.MsSqlServer.Commands.Interfaces;
+
+namespace Loki.DbCopy.MsSqlServer.Factories;
+
+public interface IMsSqlDbCopyCommandFactory
+{
+    IDatabaseCopyCommand CreateCopySchemasCommand();
+    
+    IDatabaseCopyCommand CreateDropDatabaseCommand();
+    
+    IDatabaseCopyCommand CreateCreateDatabaseCommand();
+}
---
+++ Loki.DbCopy.MsSqlServer/Factories/MsSqlDbCopyCommandFactory.cs
@@ -0,0 +1,26 @@
+﻿using Loki.DbCopy.MsSqlServer.Commands;
+using Loki.DbCopy.MsSqlServer.Commands.Interfaces;
+
+namespace Loki.DbCopy.MsSqlServer.Factories;
+
+internal class MsSqlDbCopyCommandFactory(IEnumerable<IDatabaseCopyCommand?> databaseCopyCommands)
+    : IMsSqlDbCopyCommandFactory
+{
+    public IDatabaseCopyCommand CreateCopySchemasCommand()
+    {
+        return databaseCopyCommands
+            .FirstOrDefault(databaseCopyCommand => databaseCopyCommand!.GetType() == typeof(CopySchemasCommand))!;
+    }
+    
+    public IDatabaseCopyCommand CreateDropDatabaseCommand()
+    {
+        return databaseCopyCommands
+            .FirstOrDefault(databaseCopyCommand => databaseCopyCommand!.GetType() == typeof(DropDatabaseCommand))!;
+    }
+    
+    public IDatabaseCopyCommand CreateCreateDatabaseCommand()
+    {
+        return databaseCopyCommands
+            .FirstOrDefault(databaseCopyCommand => databaseCopyCommand!.GetType() == typeof(CreateDatabaseCommand))!;
+    }
+}
